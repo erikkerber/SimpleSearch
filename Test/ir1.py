bc929206898dd8b62f519a513b4097e14821f5b0
Erik Kerber <erik.kerber@gmail.com>
2011-09-21 08:18:47 -0500

    More test framework for homework

--- Test/ir1.py
+++ Test/ir1.py
@@ -1,17 +1,17 @@
+from collections import defaultdict
+
 # Debug flag: True=print intermediate data structures
 DEBUG = True
 
 # hardcoded (for simplicity) tuple of documents to index
 documents = ('D1.txt','D2.txt','D3.txt')
 
-index={}	# dictionary key: (document, term), value: frequencies/weights
+index=defaultdict(int)	# dictionary key: (document, term), value: frequencies/weights
 for document in documents:
 	for line in open( document, 'r'):
 		terms = line.split()
 		for term in terms:
 			if term != '':		# Ignore null results from split at start/end of line
-				if not index.has_key( ( document, term.lower() ) ):
-					index[ ( document, term.lower() ) ] = 0
 				index[ ( document, term.lower() ) ] += 1
 
 if DEBUG:
--- simplesearch.py
+++ simplesearch.py
@@ -1,11 +1,6 @@
 from collections import defaultdict
 import math
 
-D1 = 'The quick brown fox jumps over a lazy brown dog'
-D2 = 'Waltz, nymph, for quick jigs vex Bud'
-D3 = 'How quickly daft jumping zebras vex'
-Q = ' QUICK BROWN VEX ZEBRAS'
-
 class Document:
 	
 	"""
@@ -117,29 +112,4 @@ def dot_product(d1, d2, document_collection):
 	return total
 
 
-# Test data for homework 2.
-
-d1 = Document(D1)
-d2 = Document(D2)
-d3 = Document(D3)
-q = Document(Q)
-doc_collection = DocumentCollection({ d1, d2, d3 })
-
-#for doc in doc_collection:
-#	print '********'
-#	for word in doc.document:
-#		print word
-
-
-print 'D3 Weights - '
-for word, weight in doc_collection.get_weights(d3).items():
-	print "{0}	{1}".format(word, weight)
-	
-print ''
-
-
-
-print 'Q to D1', similarity(q, d1, doc_collection)
-print 'Q to D2', similarity(q, d2, doc_collection)
-print 'Q to D3', similarity(q, d3, doc_collection)
 
